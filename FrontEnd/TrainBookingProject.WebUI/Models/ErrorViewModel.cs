063b8cb7b8dde18512c50ca0e8e387a1b321c8e5
Muhammet Dobaoğlu <muhammetdobaoglu@hotmail.com>
2023-04-28 17:49:46 +0300

    Add project files.

---
+++ FrontEnd/TrainBookingProject.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainBookingProject.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
