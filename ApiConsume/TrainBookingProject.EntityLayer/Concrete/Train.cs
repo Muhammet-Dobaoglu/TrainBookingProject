2fe8d0e04bb8f434503296e09aedbd7bd10f59c9
Muhammet Dobaoğlu <muhammetdobaoglu@hotmail.com>
2023-04-28 18:17:47 +0300

    Entity

---
+++ ApiConsume/TrainBookingProject.EntityLayer/Concrete/Train.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainBookingProject.EntityLayer.Concrete
+{
+    public class Train
+    {
+        public int TrainID { get; set; }
+        public int TrainName { get; set; }
+        public int BookingCount { get; set; }
+        public bool DifferentCarriage { get; set; }
+
+        public List<Wagon> Wagons { get; set; }
+    }
+}
+}
---
+++ ApiConsume/TrainBookingProject.EntityLayer/Concrete/Wagon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainBookingProject.EntityLayer.Concrete
+{
+    public class Wagon
+    {
+        public int WagonID { get; set; }
+        public string WagonName { get; set; }
+        public int WagonCapacity { get; set; }
+        public int SeatCount { get; set; }
+    }
+}
